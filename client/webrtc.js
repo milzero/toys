f4be848a71fd479f2505ce89a17f19c452f59d81
milzero <yilelu@hotmail.com>
2021-12-16 01:00:48 +0800

    update

--- client/webrtc.js
+++ client/webrtc.js
@@ -229,27 +229,37 @@ function errorHandler(error) {
     console.log(error);
 }
 
+
 function getStatus() {
-    if(peerConnection == null){
-        console.error("peer connect is null")
-        return
+    if (peerConnection == null) {
+        console.error('peer connect is null');
+        return;
     }
 
-    peerConnection.getStats(peerConnection.streams[0]).then(stats => {
-        let statsOutput = "status";
-        console.log(stats)
-        stats.forEach(report => {
-            statsOutput += `<h2>Report: ${report.type}</h2>\n<strong>ID:</strong> ${report.id}<br>\n` +
-                `<strong>Timestamp:</strong> ${report.timestamp}<br>\n`;
+    let senders = peerConnection.getSenders();
+    console.log('size of senders is ', senders.length);
+    peerConnection.getStats().then(function () {
+
+        (async () => {
+            let statsOutput = '';
+            const report = await peerConnection.getStats();
+            for (let dictionary of report.values()) {
+                console.log(dictionary)
+                // statsOutput = '<p>' + dictionary.type + '</p>';
+                // statsOutput = statsOutput + '<p>' + dictionary.type + '</p>';
+                // statsOutput = statsOutput + '<p>' + '  timestamp: ' + dictionary.timestamp + '</p>';
+                // Object.keys(dictionary).forEach(key => {
+                //     if (key != 'type' && key != 'id' && key != 'timestamp') {
+                //         statsOutput = statsOutput + '<p>' + '  ' + key + ': ' + dictionary[key] + '</p>'
+                //     }
+                // });
+            }
+
+            document.getElementById('.stats-box').innerHTML = statsOutput;
+            console.log(statsOutput)
+        })();
 
 
-            Object.keys(report).forEach(statName => {
-                if (statName !== "id" && statName !== "timestamp" && statName !== "type") {
-                    statsOutput += `<strong>${statName}:</strong> ${report[statName]}<br>\n`;
-                }
-            });
-        });
+    })
 
-        document.querySelector(".stats-box").innerHTML = statsOutput;
-    });
 }
